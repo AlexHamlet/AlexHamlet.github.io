5488a40524d794840878d0c8bda620dff997ec55
Alex Hamlet <alexhamlet@comcast.net>
2016-07-08 05:24:28 +0000

    Update to Clock

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -1,24 +1,61 @@
-var opts = {
-  lines: 12 // The number of lines to draw
-, length: 56 // The length of each line
-, width: 10 // The line thickness
-, radius: 42 // The radius of the inner circle
-, scale: 1 // Scales overall size of the spinner
-, corners: 1 // Corner roundness (0..1)
-, color: '#000' // #rgb or #rrggbb or array of colors
-, opacity: 0.25 // Opacity of the lines
-, rotate: 0 // The rotation offset
-, direction: 1 // 1: clockwise, -1: counterclockwise
-, speed: 1 // Rounds per second
-, trail: 60 // Afterglow percentage
-, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-, zIndex: 2e9 // The z-index (defaults to 2000000000)
-, className: 'spinner' // The CSS class to assign to the spinner
-, top: '50%' // Top position relative to parent
-, left: '50%' // Left position relative to parent
-, shadow: false // Whether to render a shadow
-, hwaccel: false // Whether to use hardware acceleration
-, position: 'absolute' // Element positioning
-};
-var target = document.getElementById('foo');
-var spinner = new Spinner(opts).spin(target);
+(function() {
+    
+    //trig lists
+    var half = .5;
+    var frac = (Math.sqrt(3)) / 2;
+    var posx = [half, frac, 1, frac, half, 0, -half, -frac, -1, -frac, -half, 0];
+    var posy = [frac, half, 0, -half, -frac, -1, -frac, -half, 0, half, frac, 1];
+    
+    //draws inner circle
+    var sc = document.getElementById("seconds");
+    var sctx = sc.getContext("2d");
+    sctx.beginPath();
+    var sRadius = 138;
+    sctx.arc(150, 150, 150, 0, 2 * Math.PI);
+    sctx.stroke();
+    initSeconds(); 
+    
+    //draws middle circle
+    var mc = document.getElementById("minutes");
+    var mctx = mc.getContext("2d");
+    mctx.beginPath();
+    var mRadius = 238;
+    mctx.arc(250, 250, 250, 0, 2 * Math.PI);
+    mctx.stroke();
+    initMinutes();
+    
+    //draws outer circle
+    var hc = document.getElementById("hours");
+    var hctx = hc.getContext("2d");
+    hctx.beginPath();
+    var hRadius = 338;
+    hctx.arc(350, 350, 350, 0, 2 * Math.PI);
+    hctx.stroke();
+    initHours();
+
+    
+               
+               //ctx.font = "25px Arial";
+               //(--> rotate method: ctx.rotate(Math.PI*2/(i*6));)
+//
+// :) I'm sure it was :P
+    //<insert acknowledgement that 12 is less than 60 here>
+    function initSeconds() {
+        for (var i = 1; i <= 12; i++) {
+            sctx.fillText(i * 5, posx[i - 1] * sRadius + 150, -(posy[i - 1] * sRadius - 150));
+        }
+    }
+
+    function initMinutes() {
+        for (var i = 1; i <= 12; i++) {
+            mctx.fillText(i * 5, posx[i - 1] * mRadius + 250, -(posy[i - 1] * mRadius - 250));
+        }
+    }
+
+    function initHours() {
+        for (var i = 1; i <= 12; i++) {
+            hctx.fillText(i,posx[i - 1] * hRadius + 350, -(posy[i - 1] * hRadius - 350));
+        }
+    }
+
+})();
