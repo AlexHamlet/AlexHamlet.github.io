31bcd60af31950e6d0f8997a77d4907b7e818258
Alex Hamlet <alexhamlet@comcast.net>
2016-09-02 03:11:45 +0000

    Fixed the sort, mean, median, and mode

--- MMMRCalculator/MMRCalculator.js
+++ MMMRCalculator/MMRCalculator.js
@@ -2,31 +2,84 @@ var list;
 var sortedList;
 var mean = 0;
 var median = 0;
-var mode = 0;
+var mode = [];
 var range = 0;
 
+document.querySelector('#list').addEventListener('keypress', function(e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+        finish();
+    }
+});
 
 function finish() {
+
     list = document.getElementById('list').value;
     sortedList = list.split(",");
     sortedList = mergeSort(sortedList);
+
+    mean = meanFind();
+    median = medianFind();
+    modeFind();
     range = sortedList[sortedList.length - 1] - sortedList[0];
-    mean = meanFind(sortedList);
-    median = sortedList[Math.floor(sortedList.length/2)];
+
     document.getElementById('list').value = sortedList;
     document.getElementById('mean').innerHTML = mean;
     document.getElementById('median').innerHTML = median;
     document.getElementById('mode').innerHTML = mode;
     document.getElementById('range').innerHTML = range;
-    console.log("Hello");
+    //console.log("Hello");
 
 }
 
-function meanFind(){
-    for(var i = 0; i < sortedList.length - 1;i++){
-        mean += sortedList.length;
+function meanFind() {
+    mean = 0;
+    for (var i = 0; i < sortedList.length; i++) {
+        mean += parseInt(sortedList[i]);
     }
-    return mean/sortedList.length;
+    return mean / sortedList.length;
+}
+
+function medianFind() {
+    if (sortedList.length % 2 == 0) {
+        var top = parseInt(sortedList[sortedList.length / 2]);
+        var bottom = parseInt(sortedList[sortedList.length / 2 - 1]);
+        return (bottom + top) / 2;
+    }
+    else {
+        return sortedList[Math.floor(sortedList.length / 2)];
+    }
+}
+
+function modeFind() {
+    mode = [];
+    for (var p = 0; p < sortedList.length; p++) {
+        var count = 0;
+        for (var s = 0; s < sortedList.length; s++) {
+            if (sortedList[p] == sortedList[s]) {
+                count++;
+            }
+        }
+        var insert = [sortedList[p], count];
+        mode.push(insert);
+    }
+
+    do {
+        var comb = 0;
+        for (var i = 0; i < mode.length - 1; i++) {
+            if (mode[i][1] < mode[i + 1][1] || mode[i][0] == mode[i + 1][0]) {
+                mode.splice(i, 1);
+                comb++;
+            }
+            else if (mode[i][1] > mode[i + 1][1]) {
+                mode.splice(i + 1, 1);
+                comb++;
+            }
+        }
+    } while (comb != 0)
+    for (var i = 0; i < mode.length; i++) {
+        mode[i] = mode[i][0];
+    }
 }
 
 function merge(left, right) {
@@ -35,7 +88,7 @@ function merge(left, right) {
         ir = 0;
 
     while (il < left.length && ir < right.length) {
-        if (left[il] < right[ir]) {
+        if (parseInt(left[il]) < parseInt(right[ir])) {
             result.push(left[il++]);
         }
         else {
