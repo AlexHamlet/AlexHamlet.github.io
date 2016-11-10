886aa260e4bd698d4043c0c5e57cc2006e65b7d1
Alex Hamlet <alexhamlet@comcast.net>
2016-11-10 16:58:54 +0000

    Fixed +1 error on numbers generated

--- RandomNumberGen/RandomNumberGen.js
+++ RandomNumberGen/RandomNumberGen.js
@@ -1,16 +1,18 @@
 document.body.addEventListener('keypress', function(e) {
     var key = e.which || e.keyCode;
     if (key === 13) {
-    var low = document.getElementById("low").value;
-    var high = document.getElementById("high").value;
-    var diff = parseFloat(high) - parseFloat(low);
-    var many = document.getElementById("many").value;
-    document.getElementById('numbers').innerHTML = "";
-    for (var i = 0; i < many; i++) {
-        if(i == 0){
-            document.getElementById("numbers").innerHTML += " " + Math.floor(Math.random() * (diff + 1) + parseFloat(low));
+        var low = document.getElementById("low").value;
+        var high = document.getElementById("high").value;
+        var diff = parseFloat(high) - parseFloat(low);
+        var many = document.getElementById("many").value;
+        document.getElementById('numbers').innerHTML = "";
+        for (var i = 0; i < many; i++) {
+            if (i == 0) {
+                document.getElementById("numbers").innerHTML += " " + Math.floor(Math.random() * (diff + 1) + parseFloat(low));
+            }
+            else {
+                document.getElementById("numbers").innerHTML += ", " + Math.floor(Math.random() * (diff + 1) + parseFloat(low));
+            }
         }
-        document.getElementById("numbers").innerHTML += ", " + Math.floor(Math.random() * (diff + 1) + parseFloat(low));
-    }
     }
 });
