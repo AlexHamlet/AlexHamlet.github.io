f4e69501c302391cd50c875b7a53bb792412317d
Alex Hamlet <alexhamlet@comcast.net>
2016-11-11 20:39:45 +0000

    Added decimal functions

--- RandomNumberGen/RandomNumberGen.js
+++ RandomNumberGen/RandomNumberGen.js
@@ -1,17 +1,45 @@
 document.body.addEventListener('keypress', function(e) {
     var key = e.which || e.keyCode;
     if (key === 13) {
-        var low = document.getElementById("low").value;
-        var high = document.getElementById("high").value;
-        var diff = parseFloat(high) - parseFloat(low);
-        var many = document.getElementById("many").value;
-        document.getElementById('numbers').innerHTML = "";
-        for (var i = 0; i < many; i++) {
-            if (i == 0) {
-                document.getElementById("numbers").innerHTML += " " + Math.floor(Math.random() * (diff + 1) + parseFloat(low));
+        if (document.getElementById("int").checked) {
+            if (document.getElementById("low").value < document.getElementById("high").value) {
+                var low = document.getElementById("low").value;
+                var high = document.getElementById("high").value;
             }
             else {
-                document.getElementById("numbers").innerHTML += ", " + Math.floor(Math.random() * (diff + 1) + parseFloat(low));
+                var high = document.getElementById("low").value;
+                var low = document.getElementById("high").value;
+            }
+            var diff = parseFloat(high) - parseFloat(low);
+            var many = document.getElementById("many").value;
+            document.getElementById('numbers').innerHTML = "";
+            for (var i = 0; i < many; i++) {
+                if (i == 0) {
+                    document.getElementById("numbers").innerHTML += " " + Math.floor(Math.random() * (diff + 1) + parseFloat(low));
+                }
+                else {
+                    document.getElementById("numbers").innerHTML += ", " + Math.floor(Math.random() * (diff + 1) + parseFloat(low));
+                }
+            }
+        }else{
+            if (document.getElementById("low").value < document.getElementById("high").value) {
+                var low = document.getElementById("low").value;
+                var high = document.getElementById("high").value;
+            }
+            else {
+                var high = document.getElementById("low").value;
+                var low = document.getElementById("high").value;
+            }
+            var diff = parseFloat(high) - parseFloat(low);
+            var many = document.getElementById("many").value;
+            document.getElementById('numbers').innerHTML = "";
+            for (var i = 0; i < many; i++) {
+                if (i == 0) {
+                    document.getElementById("numbers").innerHTML += " " + Math.floor((Math.random() * (diff + 1) + parseFloat(low)) * 100) / 100;
+                }
+                else {
+                    document.getElementById("numbers").innerHTML += ", " + Math.floor((Math.random() * (diff + 1) + parseFloat(low)) * 100) / 100;
+                }
             }
         }
     }
