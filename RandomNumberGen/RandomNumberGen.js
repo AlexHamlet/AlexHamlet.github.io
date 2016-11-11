793dd78a413367fd63b5042f0f088ebb13febdba
Alex Hamlet <alexhamlet@comcast.net>
2016-11-11 20:48:54 +0000

    Fixed Decimal Generation

--- RandomNumberGen/RandomNumberGen.js
+++ RandomNumberGen/RandomNumberGen.js
@@ -3,12 +3,12 @@ document.body.addEventListener('keypress', function(e) {
     if (key === 13) {
         if (document.getElementById("int").checked) {
             if (document.getElementById("low").value < document.getElementById("high").value) {
-                var low = document.getElementById("low").value;
-                var high = document.getElementById("high").value;
+                var low = Math.ceil(document.getElementById("low").value);
+                var high = Math.floor(document.getElementById("high").value);
             }
             else {
-                var high = document.getElementById("low").value;
-                var low = document.getElementById("high").value;
+                var high = Math.floor(document.getElementById("low").value);
+                var low = Math.ceil(document.getElementById("high").value);
             }
             var diff = parseFloat(high) - parseFloat(low);
             var many = document.getElementById("many").value;
@@ -35,10 +35,10 @@ document.body.addEventListener('keypress', function(e) {
             document.getElementById('numbers').innerHTML = "";
             for (var i = 0; i < many; i++) {
                 if (i == 0) {
-                    document.getElementById("numbers").innerHTML += " " + Math.floor((Math.random() * (diff + 1) + parseFloat(low)) * 100) / 100;
+                    document.getElementById("numbers").innerHTML += " " + Math.floor((Math.random() * diff + parseFloat(low)) * 100) / 100;
                 }
                 else {
-                    document.getElementById("numbers").innerHTML += ", " + Math.floor((Math.random() * (diff + 1) + parseFloat(low)) * 100) / 100;
+                    document.getElementById("numbers").innerHTML += ", " + Math.floor((Math.random() * diff + parseFloat(low)) * 100) / 100;
                 }
             }
         }
