76f694d012bd4c0a854cae0ba08193cae00cbcdf
Alex Hamlet <alexhamlet@comcast.net>
2017-06-05 19:01:07 +0000

    It Works!

--- AnimationHelper/AnimationHelper.js
+++ AnimationHelper/AnimationHelper.js
@@ -2,14 +2,19 @@
 //TODO add insert button
 //TODO add copy to next
 
-var frames, layer;
+/*Notes:
+The three dimensional array contains: Layers, Frames, and Leds.  In that order.
+The generated code abuses persistence of vision, look it up!
+*/
+
+var frames, layer, tempLayer, tempText;
 const size = 4;
 let currentFrame = 0;
 let layer0, layer1, layer2, layer3;
 let layers = [layer0, layer1, layer2, layer3];
 let layerCompile = [];
-let tempLayer;
 let totalFrames = 0;
+let generatedCode = "";
 
 for (var p = 0; p < layers.length; p++) {
     layers[p] = [];
@@ -27,10 +32,10 @@ function formBuilder() {
 
     for (var p = 0; p < size; p++) {
         layer = "Layer " + (p + 1) + "of frame " + (currentFrame + 1);
-        layer += "<div class=\"row3\"><input type=\"checkbox\" class=\"led12\" /><input type=\"checkbox\" class=\"led13\" /><input type=\"checkbox\" class=\"led14\" /><input type=\"checkbox\" class=\"led15\" /></div> " +
-            "<div class=\"row2\"><input type=\"checkbox\" class=\"led8\" /><input type=\"checkbox\" class=\"led9\" /><input type=\"checkbox\" class=\"led10\" /><input type=\"checkbox\" class=\"led11\" /></div> " +
-            "<div class=\"row1\"><input type=\"checkbox\" class=\"led4\" /><input type=\"checkbox\" class=\"led5\" /><input type=\"checkbox\" class=\"led6\" /><input type=\"checkbox\" class=\"led7\" /></div> " +
-            "<div class=\"row0\"><input type=\"checkbox\" class=\"led0\" /><input type=\"checkbox\" class=\"led1\" /><input type=\"checkbox\" class=\"led2\" /><input type=\"checkbox\" class=\"led3\" /></div> ";
+        layer += "<div class=\"row3\"><input type=\"checkbox\" class=\"led3\" /><input type=\"checkbox\" class=\"led7\" /><input type=\"checkbox\" class=\"led11\" /><input type=\"checkbox\" class=\"led15\" /></div> " +
+            "<div class=\"row2\"><input type=\"checkbox\" class=\"led2\" /><input type=\"checkbox\" class=\"led6\" /><input type=\"checkbox\" class=\"led10\" /><input type=\"checkbox\" class=\"led14\" /></div> " +
+            "<div class=\"row1\"><input type=\"checkbox\" class=\"led1\" /><input type=\"checkbox\" class=\"led5\" /><input type=\"checkbox\" class=\"led9\" /><input type=\"checkbox\" class=\"led13\" /></div> " +
+            "<div class=\"row0\"><input type=\"checkbox\" class=\"led0\" /><input type=\"checkbox\" class=\"led4\" /><input type=\"checkbox\" class=\"led8\" /><input type=\"checkbox\" class=\"led12\" /></div> ";
         document.getElementById("layer" + p).innerHTML = layer;
     }
 
@@ -68,9 +73,33 @@ function finishCode() {
         codeName = "iWouldHavePreferedAName";
     }
 
-    var generatedCode = "";
+    generatedCode = "";
+    tempText = "";
 
-    generatedCode += "void " + codeName + "() { \n" + "clear();";
+    generatedCode += "void " + codeName + "() { \n" + "clear();\n";
+    generatedCode += "for(int frames = 0;frames < " + totalFrames + "; frames++) {\n";
+    generatedCode += "for(int s = 0;s < animationtime;s++) {\n";
+    generatedCode += "for(int currentLayer = 0;currentLayer < " + layers.length + ";currentLayer++) {\n" +
+        "digitalWrite(layer[currentLayer], HIGH);\n";
+
+    for (var p = 0; p < layers.length; p++) {
+        tempText += "if(currentLayer == " + p + ") {\n";
+        for (var s = 0; s < totalFrames; s++) {
+            if (layers[p][s].some(checkTrue)) {
+                tempText += "if(frames == " + s + ") {\n";
+                for (var t = 0; t < 16; t++) {
+                    if (layers[p][s][t] == true) {
+                        tempText += "digitalWrite(led[" + Math.floor(t / 4) + "][" + (t % 4) + "], HIGH); \n";
+                    }
+                }
+                tempText += "}\n";
+            }
+        }
+        tempText += "}\n";
+    }
+    tempText += "delay(1);\nclear();\n}\n}\n}\n}\n";
+
+    generatedCode += tempText;
 
     document.getElementById("endCode").innerHTML = generatedCode;
 
@@ -88,4 +117,8 @@ function saveFrame() {
         }
         layers[p][currentFrame] = layerCompile;
     }
+}
+
+function checkTrue(value) {
+    return (value == true);
 }
